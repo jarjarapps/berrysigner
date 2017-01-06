53a8d95fcc57bc45280e8df55b1d0e92a482113b
Maciej Gibas <gibasmaciej@gmail.com>
2017-01-06 18:30:53 -0500

    Initial commit

---
+++ berrysigner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  berrysigner
+//
+//  Created by Maciej Gibas on 1/6/17.
+//  Copyright © 2017 Maciej Gibas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
