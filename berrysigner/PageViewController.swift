c7afdee1d973a3b095d15613f7b27588f65c985c
Maciej Gibas <gibasmaciej@gmail.com>
2017-01-11 21:15:21 -0500

    added page view

---
+++ berrysigner/PageViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PageViewController.swift
+//  berrysigner
+//
+//  Created by Maciej Gibas on 1/11/17.
+//  Copyright © 2017 Maciej Gibas. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.hidesBarsOnTap = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
